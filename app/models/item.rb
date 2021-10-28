c297b6568dbaf73dc66186468facffcfe0ff1a0e
mina153 <popy0125@icloud.com>
2021-10-29 00:27:27 +0900

    空のコミット

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,6 @@ validates :category_id, numericality: { other_than: 1 , message: "can't be blank
 validates :expiry, presence: true
 validates :explanation, length: { maximum: 100}
 
-extend ActiveHash::Associations::ActiveRecordExtensions
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
 end
