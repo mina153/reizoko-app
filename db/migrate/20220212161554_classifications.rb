5083fddd4bd056840769109f689db35a204b6c8e
mina153 <popy0125@icloud.com>
2022-02-13 01:16:39 +0900

    もう一度classificationsテーブル削除

--- db/migrate/20220212161554_classifications.rb
+++ db/migrate/20220212161554_classifications.rb
@@ -1,6 +1,7 @@
 class Classifications < ActiveRecord::Migration[6.0]
   def change
-    drop_table :classifications
+    def change
+      drop_table :classifications
+    end
   end
 end
-
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_01_134811) do
+ActiveRecord::Schema.define(version: 2022_02_12_161554) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "thing", null: false
